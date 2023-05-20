3bae55c574cfa643009fe6a61cf7a047cd419b8d
Voibrat <dima.voibrat@gmail.com>
2023-05-20 22:38:22 +0300

    change cmake ver

---
+++ cmake-build-debug/CMakeFiles/CourseworkFlightApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CourseworkFlightApp.dir/main.cpp.o"
+  "CourseworkFlightApp"
+  "CourseworkFlightApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CourseworkFlightApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CourseworkFlightApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CourseworkFlightApp.
+# This may be replaced when dependencies are built.
